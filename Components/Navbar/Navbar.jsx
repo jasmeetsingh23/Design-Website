288fe70662ebe2a0923bfda3455389fd60c9c693
jasmeet23 <js2365536@gmail.com>
2024-07-20 13:05:18 -0700

    initiat commit

---
+++ Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className='nav-logo'>EV-Olution</div>
+      <ul className='nav-menu'>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className='nav-contact'>Contact</li>
+      </ul>
+       
+    </div>
+  )
+}
